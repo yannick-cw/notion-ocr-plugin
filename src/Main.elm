56c3ba82e6891dabaec5f763932f93d131421d96
Yannick Gladow <yannick.gladow@holidaycheck.com>
2019-11-25 17:38:42 +0100

    Bootstrap styles

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,12 @@
 module Main exposing (main)
 
+import Bootstrap.Alert as Alert
+import Bootstrap.Button as Button
+import Bootstrap.ButtonGroup as ButtonGroup
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
 import Browser
-import Html exposing (Html, button, div, h1, img, text)
+import Html exposing (Html, button, div, h6, img, text)
 import Html.Attributes exposing (src)
 import Html.Events exposing (onClick)
 import Maybe exposing (withDefault)
@@ -15,12 +20,18 @@ import Ports exposing (askToken, notionToken)
 type alias Model =
     { token : Maybe String
     , showNoTokenFound : Bool
+    , syncState : SyncState
     }
+
+
+type SyncState
+    = SyncOff
+    | SyncOn
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { token = Nothing, showNoTokenFound = False }, Cmd.none )
+    ( { token = Nothing, showNoTokenFound = False, syncState = SyncOff }, Cmd.none )
 
 
 
@@ -30,19 +41,26 @@ init =
 type Msg
     = GotToken (Maybe String)
     | AskToken
+    | SyncState SyncState
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotToken (Just token) ->
-            ( { model | token = Just token }, Cmd.none )
+            ( { model | token = Just token, showNoTokenFound = False }, Cmd.none )
 
         GotToken Nothing ->
-            ( { model | showNoTokenFound = True }, Cmd.none )
+            ( { model | showNoTokenFound = True, syncState = SyncOff }, Cmd.none )
 
         AskToken ->
             ( model, askToken () )
+
+        SyncState SyncOff ->
+            ( { model | syncState = SyncOff }, Cmd.none )
+
+        SyncState SyncOn ->
+            ( { model | syncState = SyncOn }, askToken () )
 
 
 
@@ -54,22 +72,63 @@ view model =
     let
         errorMsg =
             if model.showNoTokenFound then
-                [ h1 [] [ text "Could not find your token, please log in to Notion and try again" ] ]
+                [ Alert.simpleDanger [] [ text "Could not find your token, please log in to Notion and try again" ] ]
 
             else
                 []
+
+        runSyncBtn =
+            div [] [ Button.button [ Button.primary, Button.onClick AskToken ] [ text "Run Once" ] ]
+
+        toggleBtns =
+            div []
+                (ButtonGroup.radioButtonGroup []
+                    [ ButtonGroup.radioButton
+                        (model.syncState == SyncOff)
+                        [ if model.syncState == SyncOff then
+                            Button.success
+
+                          else
+                            Button.secondary
+                        , Button.onClick <| SyncState SyncOff
+                        ]
+                        [ text "Sync Off" ]
+                    , ButtonGroup.radioButton
+                        (model.syncState == SyncOn)
+                        [ if model.syncState == SyncOn then
+                            Button.success
+
+                          else
+                            Button.secondary
+                        , Button.onClick <| SyncState SyncOn
+                        ]
+                        [ text "Sync On" ]
+                    ]
+                    :: errorMsg
+                )
     in
-    div []
-        ([ img [ src "/logo.svg" ] []
-         , h1 [] [ text "Your Elm App is working!" ]
-         , h1 [] [ text (withDefault "No token loaded" model.token) ]
-         , button [ onClick AskToken ] [ text "Sync Notion" ]
-         ]
-            ++ errorMsg
-        )
+    Grid.container []
+        [ CDN.stylesheet
+        , Grid.row []
+            [ Grid.col []
+                [ h6 [] [ text "Notion Image Search" ]
+                , runSyncBtn
+                , toggleBtns
+                ]
+            ]
+        ]
 
 
 
+--div
+--[]
+--([ img [ src "/logo.svg" ] []
+--, h1 [] [ text "Your Elm App is working!" ]
+--, h1 [] [ text (withDefault "No token loaded" model.token) ]
+--, button [ onClick AskToken ] [ text "Sync Notion" ]
+--]
+--++ errorMsg
+--)
 ---- Subs ----
 
 
